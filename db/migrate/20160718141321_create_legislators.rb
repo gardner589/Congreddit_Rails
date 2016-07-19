e6698367ce356d7068adae45f8ecbc0f85611451
Dave Corderman <dcordz@gmail.com>
2016-07-19 09:43:25 -0400

    did a thing

--- db/migrate/20160718141321_create_legislators.rb
+++ db/migrate/20160718141321_create_legislators.rb
@@ -1,10 +1,12 @@
 class CreateLegislators < ActiveRecord::Migration[5.0]
   def change
     create_table :legislators do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
       t.string :party
       t.integer :year_elected
       t.string :bio_id
+      t.string :chamber
       t.integer :number
       t.timestamps
     end
