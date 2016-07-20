3d8d79978cf560d2285a35d9da98280d7a31b9e4
Dave Corderman <dcordz@gmail.com>
2016-07-20 15:33:27 -0400

    some shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # resources :votes
+
   resources :legislators do
     resources :votes
-    resources :comments
+    resources :legislator_comments
   end
-=======
-  
+
   resources :bills do
     resources :bill_comments
   end
 
   resources :legislators do
-    resources :legislator_comments
   end
 
-  resources :votes
->>>>>>> ext_api_connect
+  # resources :votes
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
