dc56f5edb97a508b757ea5f7b7e48094e5a3fdc9
Dave Corderman <dcordz@gmail.com>
2016-07-20 08:43:13 -0400

    un nested votes and legislators

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # resources :votes
-  resources :legislators do
-    resources :votes
-  end
+  resources :legislators
+  resources :votes
   resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
